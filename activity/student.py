87b8c09e06b5458ad909dc955dd3aeb1f3ee78bd
liyan <liyangeneral@gmail.com>
2025-09-25 20:22:37 -0700

    final version by liyan

--- activity/student.py
+++ activity/student.py
@@ -3,14 +3,14 @@ class Student:
     def __init__(self, name, grade, classes):
         self.name = name
         self.grade = grade
-        self.have_class = classes
+        self.class_enrolled = classes
 
     def add_class(self, new_class):
-        self.have_class.append(new_class)
-        return self.have_class     # need return or not?
+        self.class_enrolled.append(new_class)
+        return self.class_enrolled     # need return or not?
     
     def get_num_classes(self):
-        return len(self.have_class)
+        return len(self.class_enrolled)
     
     def summary(self):
         return f"{self.name} is a {self.grade} enrolled in {self.get_num_classes()} classes"      # return or print?
